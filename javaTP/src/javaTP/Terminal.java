67b52ae93f84995d0bbc64a616adb0030abb62d9
pmurga <pablomurga94@gmail.com>
2018-05-28 11:14:30 -0300

    ejemplo getDispositivo y cambios en terminal

--- javaTP/src/javaTP/Terminal.java
+++ javaTP/src/javaTP/Terminal.java
@@ -2,7 +2,17 @@ package javaTP;
 
 public abstract class Terminal implements iSO, iDispositivo
 {
+	protected IP ipHost;
+	protected String tipoDispositivo;
 	
+	public Terminal()
+	{
+		
+	}
 	
+	public void getDispositivo()
+	{
+		tipoDispositivo = "Terminal";
+	}
 
 }
