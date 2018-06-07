cd9be13bd9fc618aff893d98568f39c35ce8c278
Agurshageok <san.ferreiro92@gmail.com>
2018-06-07 10:56:03 -0300

    Emprolijada a los packs

--- javaTP/src/javaTP/ICMPRequest.java
+++ javaTP/src/javaTP/ICMPRequest.java
@@ -1,33 +1,22 @@
 package javaTP;
 
-
 import java.util.Optional;
 
-public class ICMPRequest extends PaqueteDeServicio{
+public class ICMPRequest extends PaqueteDeServicio {
 
-	/*@Override
-	public Optional<Paquete> procesarServicio(Dispositivo d, SistemaOperativo so, Paquete p) {
-		//Envía al equipo que originó el paquete un nuevo paquete de tipo ICMPResponse.
-		ICMPResponse sm_tipo = new ICMPResponse(p.getIpOrigen());
-		PaqueteDeServicio sm = new PaqueteDeServicio();
-		sm.setIpOrigen(p.getIpDestino());
-		sm.setIpDestino(p.getIpOrigen());
+	public ICMPRequest(IP orig, IP dest, Integer t) {
+		super(orig, dest, t);
+	}
+
+	@Override
+	public Optional<Paquete> procesar(Dispositivo d, SistemaOperativo so) {
+		// Envía al equipo que originó el paquete un nuevo paquete de tipo ICMPResponse.
+		ICMPResponse sm = new ICMPResponse(getIpOrigen());
+		sm.setIpOrigen(getIpDestino());
+		sm.setIpDestino(getIpOrigen());
 		sm.setTtl(so.default_ttl);
-		sm.setTipo(sm_tipo);
 		Optional<Paquete> pack = Optional.of(sm);
 		return pack;
 	}
-*/
-	@Override
-	public Optional<Paquete> procesar(Dispositivo d, SistemaOperativo so) {
-		//Envía al equipo que originó el paquete un nuevo paquete de tipo ICMPResponse.
-				ICMPResponse sm = new ICMPResponse(getIpOrigen());
-				sm.setIpOrigen(getIpDestino());
-				sm.setIpDestino(getIpOrigen());
-				sm.setTtl(so.default_ttl);
-				Optional<Paquete> pack = Optional.of(sm);
-				return pack;
-	}
 
-	
 }
--- javaTP/src/javaTP/ICMPResponse.java
+++ javaTP/src/javaTP/ICMPResponse.java
@@ -1,43 +1,33 @@
 package javaTP;
 
-
 import java.sql.Timestamp;
 
 import java.util.Optional;
 
-
-public class ICMPResponse extends PaqueteDeServicio{
+public class ICMPResponse extends PaqueteDeServicio {
 	private Timestamp timestamp;
 	private IP equip_orig;
-	
-	
+
 	public Timestamp getTimestamp() {
 		return timestamp;
 	}
+
 	public void setTimestamp(Timestamp timestamp) {
 		this.timestamp = timestamp;
 	}
-	
+
 	public ICMPResponse(IP ip) {
 		super();
 		timestamp = new Timestamp(System.currentTimeMillis());
 		equip_orig = ip;
 	}
-	/*@Override
-	public Optional<Paquete> procesarServicio(Dispositivo d, SistemaOperativo so, Paquete p) {
-		Optional<Paquete> pack = Optional.empty();
-		
-		System.out.println("Recibido ICMP desde: "+ equip_orig.toString()  + "TimeStamp: "+timestamp.toString());
-		
-		return pack;
-	}*/
+
 	@Override
 	public Optional<Paquete> procesar(Dispositivo d, SistemaOperativo so) {
 
-		
-		System.out.println("Recibido ICMP desde: "+ equip_orig.toString()  + "TimeStamp: "+timestamp.toString());
-		
+		System.out.println("Recibido ICMP desde: " + equip_orig.toString() + "TimeStamp: " + timestamp.toString());
+
 		return Optional.empty();
 	}
-	
+
 }
